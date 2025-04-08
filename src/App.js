b4faefc67f1466649d298d587d7df6a5d82e9b8a
dhrumil_mandaviya <dhrumilmandaviya464@gmail.com>
2025-04-08 21:48:15 +0530

    dependency install and hello world

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='font-sans text-center text-blue-600 text-3xl bg-slate-400 border-4 rounded-full border-pink-600'>Hello World</h1>
     </div>
   );
 }
